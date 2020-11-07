7c00f4902e6f3f12334c0f36addad21ce8ac4dfa
hatori-r <aruue368@gmail.com>
2020-11-07 14:23:04 +0900

    commit

---
+++ app/controllers/tasks/searches_controller.rb
@@ -0,0 +1,2 @@
+class Tasks::SearchesController < ApplicationController
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,4 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :following, :followers]
+  
 end
